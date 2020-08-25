63e811e9a6bd014380c84850db5af06f69b37626
Taylor Johnson <taylorjohnson141@gmail.com>
2020-08-25 14:37:28 -0600

    Add returnAverergeUserFluidOuncesConsumedAllTime method

--- src/Hydration.js
+++ src/Hydration.js
@@ -2,5 +2,14 @@ class Hydration {
     constructor(HydrationData){
         this.HydrationData = HydrationData
     }
+    returnAverergeUserFluidOuncesConsumedAllTime(UserId){
+        let singleUserData = this.HydrationData.filter(user => { 
+            return user.userID === UserId})
+        let totalOuncesDrank = singleUserData.reduce((startingValue,user)=>{
+                return  startingValue += user.numOunces
+        },0)
+        return totalOuncesDrank / singleUserData.length
+    }
+
 }
 module.exports = Hydration
