73080da0750ec99cca68e2079b63575057f97e58
Nathan Darrington <npdarrington@icloud.com>
2020-08-25 16:33:55 -0600

    Populate currentUser onload for testing

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+let currentUser;
+
+const getCurrentUser = userData => {
+  currentUser = userData.find(user => user.id === 1);
+}
+
+
+
+window.onload = getCurrentUser(userData);
+
+
+// console.log("Hello World");
