b72d1bd63a7df3c24f6627090cb3180797646d9c
Taylor Johnson <taylorjohnson141@gmail.com>
2020-08-25 14:52:21 -0600

    returnFluidOuncesForSpecificDay method

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -4,18 +4,18 @@ const Hydration = require('../src/Hydration.js')
 const hydrationTestData = require('./hydrationTestData.js')
 let hydration;
 beforeEach(function(){
-    hydration = new Hydration()
+    hydration = new Hydration(hydrationTestData)
 })
 describe('Hydration',()=>{
     it('Should return average fluid ounces consumed per day for all time',()=>{
-        expect(Hydration.returnAverergeUserFluidOuncesConsumedAllTime(UserID)).to.equal()
+        expect(hydration.returnAverergeUserFluidOuncesConsumedAllTime(1)).to.equal(59)
     })
     it('should return how many fluid ounces a user consumed for a specific day ',()=>{
-        expect(Hydration.returnAveragefluidOuncesForSpecificDay(UserID,Day)).to.equal()
+        expect(hydration.returnFluidOuncesForSpecificDay(18,"2019/06/17")).to.equal()
 
     })
     it('Should return many fluid ounces of water consumed each day over the course of a week',()=>{
-        expect(Hydration.returnUserWeeklyFluidConsumption(UserID,StartingDay)).to.equal()
+        expect(hydration.returnUserWeeklyFluidConsumption(UserID,StartingDay)).to.equal()
         
     })
 })
