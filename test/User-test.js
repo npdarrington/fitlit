1d30936ae67924f5c7ddc84f5f4f645d09cc80a2
Nathan Darrington <npdarrington@icloud.com>
2020-08-24 19:13:30 -0600

    Added test for user being and instance of user class

--- test/User-test.js
+++ test/User-test.js
@@ -1,7 +1,17 @@
 const User = require('../src/User');
+const userData = require('./userTestData');
+const { expect } = require('chai');
 
 describe('User', () => {
+  let userId;
+  let user1 = new User();
   beforeEach(() => {
-    
+    userId = userData.find(user => user.id === 1);
+    user1 = new User(userId);
+  });
+
+  it('should be an instance of User', () => {
+    user1 = new User(userData.id === 1);
+    expect(user1).to.be.an.instanceof(User);
   });
 });
