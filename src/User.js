b42be9b361ad5d62526edd0e05538f00c44b8952
Nathan Darrington <npdarrington@icloud.com>
2020-08-24 20:24:51 -0600

    Refactored getUserName to only return first name

--- src/User.js
+++ src/User.js
@@ -2,6 +2,10 @@ class User {
   constructor(userData) {
     this.userData = userData;
   }
+
+  getUserName() {
+    return this.userData.name.substr(0, this.userData.name.indexOf(' '));
+  }
 }
 
 if (typeof module !== 'undefined') {
