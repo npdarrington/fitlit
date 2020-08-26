6cb8aea246dfbea9c0b671745ade077f838e6ef5
Nathan Darrington <npdarrington@icloud.com>
2020-08-26 13:44:27 -0600

    Added require statements for testing

---
+++ test/Sleep-test.js
@@ -0,0 +1,5 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sleepData = require('./sleepTestData');
+const userData = require('./userTestData');
+const Sleep = require('../src/Sleep');
