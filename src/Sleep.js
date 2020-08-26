20dcdcaecb78cb13c8f351d4856c82508578e6a5
Nathan Darrington <npdarrington@icloud.com>
2020-08-26 14:52:35 -0600

    Create getUserAverageSleptHoursPerDay method

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,6 +1,20 @@
 class Sleep {
   constructor(sleepData) {
-    this.sleep = sleepData;
+    this.sleepData = sleepData;
+  }
+
+  getUserAverageSleptHoursPerDay(userId) {
+    let singleUserData = this.sleepData.filter(user => {
+      return user.userID === userId;
+    });
+    let userSleepPerDayAverage = singleUserData.reduce((total, user) => {
+      return total += user.hoursSlept;
+    }, 0);
+    let averageUserSleepHours = userSleepPerDayAverage / singleUserData.length;
+    let getDecimalNumber = averageUserSleepHours.toString().split('.');
+    let turnDecimalIntoMinutes = Math.round(`.${getDecimalNumber[1]}` * 60);
+    let finalNumber = +(`${getDecimalNumber[0]}.${turnDecimalIntoMinutes}`);
+    return finalNumber;
   }
 }
 
