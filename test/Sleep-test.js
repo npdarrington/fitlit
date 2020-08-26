49b87d2e9f8c957935ce5ee890f2488fab789b40
Nathan Darrington <npdarrington@icloud.com>
2020-08-26 13:47:04 -0600

    Set up describe and beforeEach block for testing

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -3,3 +3,10 @@ const expect = chai.expect;
 const sleepData = require('./sleepTestData');
 const userData = require('./userTestData');
 const Sleep = require('../src/Sleep');
+
+describe('Sleep', () => {
+  let sleep;
+  beforeEach(() => {
+    sleep = new Sleep(sleepData);
+  });
+});
