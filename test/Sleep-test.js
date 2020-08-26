25029094632b2687333c88675cfc4b4a8a292d2a
Nathan Darrington <npdarrington@icloud.com>
2020-08-26 14:59:13 -0600

    Added test for user average sleep quality over all time

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -12,4 +12,8 @@ describe('Sleep', () => {
   it('Should get a user\'s verage number of hours slept per day', () => {
     expect(sleep.getUserAverageSleptHoursPerDay(1)).to.equal(7.34)
   });
+
+  it('Should should show a user their average sleep quality per day over all time', () => {
+    expect(sleep.getUserAverageSleepQualityAllTime(1)).to.equal(2.7)
+  })
 });
