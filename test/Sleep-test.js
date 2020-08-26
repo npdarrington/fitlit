88ad69edeb704b817f09582b0535fe64ad12abff
Nathan Darrington <npdarrington@icloud.com>
2020-08-26 14:11:25 -0600

    Removed userData require

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 const sleepData = require('./sleepTestData');
-const userData = require('./userTestData');
 const Sleep = require('../src/Sleep');
 
 describe('Sleep', () => {
