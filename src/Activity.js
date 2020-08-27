ce01f3aa384c4ae545794cab6e18da4e17a5365a
Taylor Johnson <taylorjohnson141@gmail.com>
2020-08-27 16:01:12 -0600

    Add returnMilesWalkedForGivenDay method to Activity class

--- src/Activity.js
+++ src/Activity.js
@@ -1,8 +1,22 @@
+const User = require('./User.js')
+const userTestData = require('../test/userTestData.js')
 class Activity {
     constructor(ActivityTestData){
         this.data = ActivityTestData
     }
-    returnMilesWalkedForGivenDay(){
-        
+    returnMilesWalkedForGivenDay(userId,date){
+        const user = new User(userId)
+
+        let singleUserData = this.sleepData.filter(user => {
+            return user.userID === userId;
+          });
+         var SingleDay =  singleUserData.filter(user =>{
+              return user.date === date
+          })
+        let UserNumberStepsPerMile = 5280/user.strideLength
+          
+        let milesWalked = SingleDay.stepsTaken / UserNumberStepsPerMile
+        return milesWalked
+
     }
 }
