5a2becf1d7754526b00a253a58de32d27c0c6395
Taylor Johnson <taylorjohnson141@gmail.com>
2020-08-27 18:54:09 -0600

    Refactor Tests

--- test/activity-test.js
+++ test/activity-test.js
@@ -21,25 +21,31 @@ describe('Activity',()=>{
 
     })
     it('Should return how many minutes active did they average for a given week', ()=>{
-        expect(activity.returnAverageActiveMinutesForWeek(1,"2019/06/21"))
+        expect(activity.returnAverageActiveMinutesForWeek(1,"2019/06/21")).to.equal(140)
     })
     it('Should return if a user reached their step goal for a given day ',()=> {
         expect(activity.returnIfUserReachedStepGoalForDay(1,"2019/06/28")).to.equal(true)
     })
     it('Should return all days a user reached their step goal', ()=>{
-        expect(activity.returnAllDaysAUserReachedTheirStepGoal(1)).to.deep.equal()
+        expect(activity.returnAllDaysAUserReachedTheirStepGoal(1)).to.deep.equal([
+            { date: '2019/06/17', numSteps: 14329 },
+            { date: '2019/06/20', numSteps: 14478 },
+            { date: '2019/06/22', numSteps: 10289 },
+            { date: '2019/06/23', numSteps: 13928 },
+            { date: '2019/06/28', numSteps: 10517 }
+          ])
     })
     it('Should return their all-time stair climbing record', ()=>{
-        expect(activity.returnAlltimeStairClimbingRecord()).to.equal(39)
+        expect(activity.returnAlltimeStairClimbingRecord(1)).to.equal(39)
     })
     it('Should return average number of stairs climbed for a specified date for all user',()=>{
-        expect(activity.returnAverageNumberOfStairsClimbedForDay("2019/06/21")).to.equal()
+        expect(activity.returnAverageNumberOfStairsClimbedForDay("2019/06/21")).to.equal(20)
     })
     it('Should return average number of steps taken for a specified date for all user',()=>{
-        expect(activity.returnAverageNumberOfStepsTakenForDay("2019/06/21")).to.equal()
+        expect(activity.returnAverageNumberOfStepsTakenForDay("2019/06/21")).to.equal(9164)
     })
     it('Should return average number of minutes active for a specified date for all user',()=>{
-        expect(activity.returnAverageNumberOfMinutesActiveForDay("2019/06/21")).to.equal()
+        expect(activity.returnAverageNumberOfMinutesActiveForDay("2019/06/21")).to.equal(157)
     })
     
 })
