d51a8ae6c2969557684f3bac7284ae78348ed8e4
Nathan Darrington <npdarrington@icloud.com>
2020-08-29 16:10:21 -0600

    Refactor for eslint consistency

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -1,22 +1,25 @@
-const chai = require('chai')
-const expect = chai.expect
-const Hydration = require('../src/Hydration.js')
-const hydrationTestData = require('./hydrationTestData.js')
+const chai = require('chai');
+const expect = chai.expect;
+const Hydration = require('../src/Hydration.js');
+const hydrationTestData = require('./hydrationTestData.js');
 let hydration;
-beforeEach(function(){
-    hydration = new Hydration(hydrationTestData)
-})
-describe('Hydration',()=>{
-    it('Should return average fluid ounces consumed per day for all time',()=>{
-        expect(hydration.returnAverergeUserFluidOuncesConsumedAllTime(1)).to.equal(59)
-    })
-    it('should return how many fluid ounces a user consumed for a specific day ',()=>{
-        expect(hydration.returnFluidOuncesForSpecificDay(18,"2019/06/17")).to.equal(76)
 
-    })
-    it('Should return fluid ounces of water consumed each day over the course of a week',()=>{
-        expect(hydration.returnUserWeeklyFluidConsumption(1,"2019/06/15")).to.deep.equal(
-          [
+beforeEach(() => {
+  hydration = new Hydration(hydrationTestData);
+});
+
+describe('Hydration', () =>{
+  it('Should return average fluid ounces consumed per day for all time', () => {
+    expect(hydration.returnAverergeUserFluidOuncesConsumedAllTime(1)).to.equal(59);
+  });
+  
+  it('should return how many fluid ounces a user consumed for a specific day ', () => {
+    expect(hydration.returnFluidOuncesForSpecificDay(18, "2019/06/17")).to.equal(76);
+  });
+
+  it('Should return fluid ounces of water consumed each day over the course of a week', () => {
+    expect(hydration.returnUserWeeklyFluidConsumption(1, "2019/06/15")).to.deep.equal(
+      [
         {date: "2019/06/21", numOunces: 50},
         {date: "2019/06/20", numOunces: 50},
         {date: "2019/06/19", numOunces: 91},
@@ -24,6 +27,7 @@ describe('Hydration',()=>{
         {date: "2019/06/17", numOunces: 96},
         {date: "2019/06/16", numOunces: 69},
         {date: "2019/06/15", numOunces: 37},
-        ])
-    })
-})
+      ]
+    );
+  });
+});
