f9264100868a60be0f2979984a6cc245d4878e9e
Nathan Darrington <npdarrington@icloud.com>
2020-08-29 16:09:32 -0600

    Refactor for eslint consistency

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,40 +1,38 @@
 class Hydration {
-    constructor(HydrationData){
-        this.HydrationData = HydrationData
-    }
-    returnAverergeUserFluidOuncesConsumedAllTime(UserId){
-        let singleUserData = this.HydrationData.filter(user => { 
-            return user.userID === UserId})
-        let totalOuncesDrank = singleUserData.reduce((startingValue,user)=>{
-                return  startingValue += user.numOunces
-        },0)
-        return totalOuncesDrank / singleUserData.length
-    }
-    returnFluidOuncesForSpecificDay(userId,date){
-    let singleUserData = this.HydrationData.filter(user => { 
-            return user.userID === userId
-        })
-    return singleUserData.find(user => {
-        return user.date === date}).numOunces
+  constructor(HydrationData) {
+    this.HydrationData = HydrationData;
+  }
+	
+  returnAverergeUserFluidOuncesConsumedAllTime(UserId) {
+    const singleUserData = this.HydrationData.filter(user => { 
+      return user.userID === UserId;
+    });
+    const totalOuncesDrank = singleUserData.reduce((startingValue, user)=> {
+      return startingValue += user.numOunces;
+    }, 0);
+    return totalOuncesDrank / singleUserData.length;
+  }
 
-}
+  returnFluidOuncesForSpecificDay(userId, date) {
+    const singleUserData = this.HydrationData.filter(user => { 
+      return user.userID === userId;
+    });
+    return singleUserData.find(user => user.date === date).numOunces;
+  }
 
-returnUserWeeklyFluidConsumption(UserId,startDate){
-    let singleUserData = this.HydrationData.filter(user => { 
-        return user.userID === UserId
-    })
-   var startDateObject =  singleUserData.find(user => user.date === startDate )
-   let indexOfstartDateObject = singleUserData.indexOf(startDateObject)
-   var UsernumOunces = singleUserData.map(user =>{
-       var dataAndNumOfOuncesDrank = {}
-       dataAndNumOfOuncesDrank.date  = user.date;
-       dataAndNumOfOuncesDrank.numOunces =  user.numOunces;
-             return dataAndNumOfOuncesDrank
-   })
-   return UsernumOunces.splice(indexOfstartDateObject,7).reverse()
-}
+  returnUserWeeklyFluidConsumption(UserId, startDate) {
+    const singleUserData = this.HydrationData.filter(user => { 
+      return user.userID === UserId;
+    });
+    const startDateObject = singleUserData.find(user => user.date === startDate);
+    const indexOfstartDateObject = singleUserData.indexOf(startDateObject);
+    const UsernumOunces = singleUserData.map(user => {
+      return { date: user.date, numOunces: user.numOunces };
+    });
+    return UsernumOunces.splice(indexOfstartDateObject, 7).reverse();
+  }
 }
 
 if (typeof module !== 'undefined') {
-    module.exports = Hydration;
+  module.exports = Hydration;
 }
