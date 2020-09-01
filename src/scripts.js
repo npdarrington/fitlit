2af6847564c9d63eb04485b0927d4c53de2f73d6
Nathan Darrington <npdarrington@icloud.com>
2020-09-01 15:50:11 -0600

    Removed unused query selectors with new UI to reassemble data

--- src/scripts.js
+++ src/scripts.js
@@ -1,29 +1,6 @@
-let userTitle = document.querySelector('.user-title');
-let userName = document.querySelector('.name');
-let userAddress = document.querySelector('.address');
-let userEmail = document.querySelector('.email');
-let userFriends = document.querySelector('.friends');
-let userStrideLength = document.querySelector('.stride-length');
-let userStepGoal = document.querySelector('.user-step-count');
-let allUsersStepGoal = document.querySelector('.all-users-step-count');
-let waterDrankToday = document.querySelector('.calculated-water-drank')
-let weeklyWaterIntake =  document.querySelector('.weekly-hydration')
-let dailyHoursSlept = document.querySelector('.daily-hours-slept');
-let dailyQualitySleep = document.querySelector('.daily-quality-sleep');
-let weeklyHoursSlept = document.querySelector('.weekly-hours-slept');
-let weeklyQualitySleep = document.querySelector('.weekly-quality-sleep');
-let allTimeHoursSlept = document.querySelector('.all-time-hours-slept');
-let allTimeQualitySleep = document.querySelector('.all-time-quality-sleep');
-let dailyStepCount = document.querySelector('.daily-step-count')
-let dailyMinutesActive = document.querySelector('.daily-minutes-active')
-let dailyMilesWalked  = document.querySelector('.daily-miles-walked')
-let dailyStepsLeaderBoard = document.querySelector('.number-of-steps-leaderboard')
-let dailyminutesActiveLeaderBoard = document.querySelector('.minutes-active-leaderboard')
-let dailyStairsClimbed = document.querySelector('.stairs-climbed-leaderboard')
-let stepsWeeklyOverview = document.querySelector('.steps-weekly-overview')
-let minutesActiveWeeklyOverview = document.querySelector('.minutes-active-weekly-overview')
-let flightsOfStairsWeeklyOverview = document.querySelector('.flights-of-stairs-weekly-overview')
-let friendLeaderBoard = document.querySelector('.friend-stepcount-leaderboard')
+let challengeBoardTitle = document.querySelector('.challenge-board-title');
+let displayFriendsResultsHead = document.querySelector('.display-friends-results > thead');
+let displayFriendsResultsBody = document.querySelector('.display-friends-results > tbody');
 let hydrationGraph = document.querySelector('#hydration-stats');
 let currentUser;
 let indexOfUser  = 1
@@ -82,8 +59,7 @@ const getUserWeeklySleepQuality = () => {
   sleepQualityData.forEach((user, index) => {
     user.hoursSlept = sleepHoursSleptData[index].hoursSlept;
   });
-  let changeChallengeBoardTitle = document.querySelector('.challenge-board-title');
-  changeChallengeBoardTitle.innerText = 'Weekly Sleep Stats';
+  challengeBoardTitle.innerText = 'Weekly Sleep Stats';
   playWithTheTable(sleepQualityData);
 }
 
@@ -205,9 +181,6 @@ const getUserWeeklySleepQuality = () => {
 // }
 
 const playWithTheTable = allStats => {
-  console.log(allStats);
-  let displayFriendsResultsHead = document.querySelector('.display-friends-results > thead');
-  let displayFriendsResultsBody = document.querySelector('.display-friends-results > tbody');
   let displayTableHead = `
     <tr><th>Date</th></tr>
     <tr><th>Hours Slept</th></tr>
@@ -271,7 +244,7 @@ const displayHydrationGraph = (hydrationStats) => {
 
 window.onload = () => {
   getCurrentUser(userData);
-  document.querySelector('.user-name').innerText = `Welcome back ${currentUser.getUserName()}`
+  document.querySelector('.user-name').innerHTML = `<span class="user-name-large">Welcome back ${currentUser.getUserName()}</span>`
   // populateUserData(currentUser);
   // getUserStepGoal(currentUser);
   // getAllUsersStepGoal();
